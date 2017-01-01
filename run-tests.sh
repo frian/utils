09cdc234feecad181a285386b5c2d78c53ab319e
frian <a@frian.org>
2017-01-01 15:38:24 +0100

    code cleaning and comments

--- run-tests.sh
+++ run-tests.sh
@@ -11,81 +11,118 @@ source ./config.cfg
 CMD="phpunit -c app/"
 
 
+# -----------------------------------------------------------------------------
+# -- FUNCTIONS
+# -----------------------------------------------------------------------------
+
 help () {
-    echo -e "\n  $0 project [testsuite]\n"
-    echo -e "      testsuite : testsuite name\n"
+    echo -e "\n  $0 [ -l -h ] -p project [testsuite]\n"
+    echo -e "      -l : list testsuites in a project\n"
+    echo -e "      -h : help\n"
     exit 1
 }
 
+# -----------------------------------------------------------------------------
+# -- VARIABLES
+# -----------------------------------------------------------------------------
+
 OPTIONS=false
 
-while getopts ":p:vl" opt; do
+
+#
+# .-- handle options
+#       -h : show help and usage
+#       -p : project name
+#       -l : list testsuites for a given project
+#
+while getopts ":p:hl" opt; do
     case $opt in
-        v)
-            HELP=true
-            OPTIONS=true
+        h)
+            help
         ;;
         l)
+            # -- set list flag
             LIST=true
+
+            # -- set options flag
             OPTIONS=true
         ;;
         p)
+            # -- check if project folder exist, if not throw error
             if [ ! -r  $BASEPATH/$OPTARG ];
             then
-                echo -e "\n  ERROR : project $1 not found or not readable in $BASEPATH/$OPTARG !\n"
+                echo -e "\n  ERROR : project $OPTARG not found or not readable in $BASEPATH/$OPTARG !\n"
                 exit 1
             fi
+
+            # -- set project flag
+            PROJECT=true
+
             # -- store project name
             PROJECTNAME=$OPTARG
 
             # -- create project path
             PROJECTPATH=$BASEPATH/$PROJECTNAME
 
+            # -- set options flag
             OPTIONS=true
         ;;
         \?)
-            echo "Invalid option: -$OPTARG" >&2
+            # -- invalid option
+            echo "  invalid option: -$OPTARG" >&2
             exit 1
         ;;
         :)
-            echo "Option -$OPTARG requires an argument." >&2
+            # -- missing argument
+            echo "  option -$OPTARG requires an argument." >&2
             exit 1
         ;;
     esac
 done
 
 
-shift $(expr $OPTIND - 1 )
-
-if [[ $1 ]]; then
-    TESTSUITE=$1
-fi
-
-
-
+# -- if no option is set show help
 if [[ $OPTIONS != "true" ]];
 then
     help
 fi
 
 
-if [[ $HELP == "true" ]]; then
-    help
+# -- get next argument
+shift $(expr $OPTIND - 1 )
+
+# -- if argument set as testsuite name
+if [[ $1 ]];
+then
+    TESTSUITE=$1
 fi
 
 
+# -----------------------------------------------------------------------------
+# -- START
+# -----------------------------------------------------------------------------
+
+# -- cd in project htdocs
+cd $PROJECTPATH/htdocs
+
+#
+# -- handle list action
+#
 if [[ $LIST == "true" ]]; then
 
-    if [[ $PROJECTNAME == "" ]];
+    #
+    # -- if project is not defined, show error and exit
+    #
+    if [[ $PROJECT != "true" ]];
     then
         echo -e "\n  option -l require -p \n"
         # help
         exit
     else
-        cd $PROJECTPATH/htdocs
-        # echo -e "\n  testsuites found in $PROJECTNAME\n"
+        # -- get testsuites
         TESTSUITES=($(grep "testsuite name" app/phpunit.xml 2>/dev/null | cut -d\" -f2 ))
 
+        # -- output
         if [[ ${#TESTSUITES[@]} -eq  0 ]]; then
             echo -e "\n  no testsuite found in $PROJECTNAME\n"
         else
@@ -102,22 +139,19 @@ if [[ $LIST == "true" ]]; then
 fi
 
 
-# -----------------------------------------------------------------------------
-# -- VARIABLES
-# -----------------------------------------------------------------------------
+#
+# -- run tests
+#
 
-
-
+# -- if testsuite append to command
 if [ $TESTSUITE ]; then
     CMD=$CMD' --testsuite '$TESTSUITE
-    echo $CMD
 fi
 
-
-# cd $PROJECTPATH
-
+# -- recreate project with Tests fixtures
 recreate.sh $PROJECTNAME Tests
 
-cd $PROJECTPATH/htdocs/
-
+# -- run phpunit
 $CMD
+
+# done
